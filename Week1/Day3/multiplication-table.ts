b239225fbc35e9a91655ef1f615a2004a046d46c
Alpar Fancsali <falpar@gmail.com>
2018-02-16 09:55:29 +0000

    Reggeli Elso

--- Week1/Day3/multiplication-table.ts
+++ Week1/Day3/multiplication-table.ts
@@ -1,25 +1,6 @@
 let number: number = 15;
-
- //Create a program that
- //prints the multiplication table with number
-//
-// Example:
-// The number 15 should print:
-//
-// 1 * 15 = 15
-// 2 * 15 = 30
-// 3 * 15 = 45
-// 4 * 15 = 60
-// 5 * 15 = 75
-// 6 * 15 = 90
-// 7 * 15 = 105
-// 8 * 15 = 120
-// 9 * 15 = 135
-// 10 * 15 = 150
-
-
-let a: number = 0;
-
-while(a < 10) {
-    console.log
+let total: number = 0;
+for(let a: number = 1; a <= 10; a++){
+    total = a * number;
+    console.log(a + " * " + number + " = " + total);
 }
