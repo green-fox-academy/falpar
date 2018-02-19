210b8757b59fe8bb3ebfdce3ff9fc05e0b4c9e70
Alpar Fancsali <falpar@gmail.com>
2018-02-19 19:25:51 +0000

    este3

--- Week2/Day1/print-all.ts
+++ Week2/Day1/print-all.ts
@@ -3,4 +3,4 @@
 // -  Log each the element of `numList` to the console*
 // *hint: use a loop, console.log(numList) won't cut it
 // -  bonus for using the correct built in array method
- let numList = [4, 5, 6, 7];
+ 
