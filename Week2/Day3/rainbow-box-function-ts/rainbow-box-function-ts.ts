52f55df0a05ef672c8513b025659e527f3ef5357
Alpar Fancsali <falpar@gmail.com>
2018-02-21 17:50:58 +0100

    purple

--- Week2/Day3/rainbow-box-function-ts/rainbow-box-function-ts.ts
+++ Week2/Day3/rainbow-box-function-ts/rainbow-box-function-ts.ts
@@ -14,18 +14,9 @@ function square(seize: number, color: string) {
 let colors: string[] = ['purple', 'violet', 'blue', 'green', 'yellow', 'orange','red'];
 
 for(let i = 6; i > 0; i--) {
-    square(i*50, colors[i]);
+    square(i*100, colors[i]);
 }
 
 
 
 
-// function rainBow(par1, par2){
-//     // ctx.fillStyle ='#'+(Math.random()*0xFFFFFF<<0).toString(16);
-//     ctx.fillRect((canvas.width / 2) - par2 / 2, (canvas.height / 2) - par2, par2, par2);
-
-// } 
-// Create a square drawing function that takes 2 parameters:
-// The square size, and the fill color,
-// and draws a square of that size and color to the center of the canvas.
-// Create a loop that fills the canvas with rainbow colored squares.
