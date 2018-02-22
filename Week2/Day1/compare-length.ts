243e22ee5279ceae26b83a5d7e3c9b525c1dab35
Alpar Fancsali <falpar@gmail.com>
2018-02-22 13:25:56 +0100

    mai elso

--- Week2/Day1/compare-length.ts
+++ Week2/Day1/compare-length.ts
@@ -6,9 +6,10 @@
 
 let firstList = [1, 2, 3];
 let secondList = [4, 5];
-if(secondList.length > firstList.length){
-console.log(firstList);
 
-}else {
+if(secondList.length > firstList.length){
+    console.log(firstList);
+
+} else {
     console.log(false);
 }
--- Week2/Day3/purple-steps-ts/purple-steps-ts.ts
+++ Week2/Day3/purple-steps-ts/purple-steps-ts.ts
@@ -8,15 +8,16 @@ const ctx = canvas.getContext('2d');
 //github.com/greenfox-academy/teaching-materials/blob/master/workshop/drawing/purple-steps/r3.png]
 //19
 
-function purpleSteps(x: number, y:number){
+function purpleSteps(x: number, y:number, ){
     ctx.fillStyle = 'purple';
-    ctx.fillRect(x, y, 15, 15);
+    ctx.fillRect(x, y,  15, 15);
 }
 let a: number = 0;
 let b: number = 0;
+let 
 
 for(let i = 0; i < 19; i++){
     a += 15;
     b += 15;
     purpleSteps(a,b);
-}
+}   
