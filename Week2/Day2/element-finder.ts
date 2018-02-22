67b3cea488ca69ba21e62e5b01389c76fe57fad8
Alpar Fancsali <falpar@gmail.com>
2018-02-22 16:35:46 +0100

    new

--- Week2/Day2/element-finder.ts
+++ Week2/Day2/element-finder.ts
@@ -2,18 +2,29 @@
 
 // Write a function that checks if the array contains "7"
 // if it contains return "Hoorray" otherwise return "Noooooo"
-const numbers: number[] = [1, 2, 3, 4, 5, 6, 8];
-function containsSeven(x) {
-    for(let i = 0; i < numbers.length; i++){
-        if (x ===}
-            
+const numbers: number[] = [1, 2, 3, 7 , 4, 5, 6, 8];
+
+function containsSeven(x: number[]) {
+    let bool: boolean = false;
+
+    for(let i = 0; i < x.length; i++ ) {
+        let sev: number = 7;
+        if(x[i] === sev){
+        bool = true;
+        };
+    }
+        if(bool === true) {
+            return'Hoorray';
+        }else{
+            return'Noooooo';
+        }
 }
 
-
 console.log(containsSeven(numbers));
+
 // The output should be: "Noooooo"
 // Do this again with a different solution using different list functions!
-export = containsSeven;
+//export = containsSeven;
 
 
 
