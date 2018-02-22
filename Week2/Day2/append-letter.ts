5232b9a604ff8239c44b0240f2d0fe1d364094d7
Alpar Fancsali <falpar@gmail.com>
2018-02-22 16:07:04 +0100

    new

--- Week2/Day2/append-letter.ts
+++ Week2/Day2/append-letter.ts
@@ -3,21 +3,21 @@
 // The parameter should be a list.
 let far: string[] = ['kuty', 'macsk', 'kacs', 'rók', 'halacsk'];
 
-// function appendA(allat: string[]) {
-//     let a:number = allat.length;
-//     for(let i = 0; i < a; i++) {
-//         allat[i]+= 'a';
-//     }
-//      return allat;
-// }
-
-//console.log(appendA(far));
-// The output should be: 'kutya', 'macska', 'kacsa', 'róka', 'halacska'
-//export = appendA;
-
 function appendA(allat: string[]) {
-    allat = allat.map(x => x + 'a');
-    return allat;
+    let a:number = allat.length;
+    for(let i = 0; i < a; i++) {
+        allat[i]+= 'a';
+    }
+     return allat;
 }
 
 console.log(appendA(far));
+//The output should be: 'kutya', 'macska', 'kacsa', 'róka', 'halacska'
+//export = appendA;
+
+// function appendA(allat: string[]) {
+//     allat = allat.map(x => x + 'a');
+//     return allat;
+// }
+
+// console.log(appendA(far));
---
+++ Week2/Day2/element-finder.ts
@@ -0,0 +1,50 @@
+'use strict';
+
+// Write a function that checks if the array contains "7"
+// if it contains return "Hoorray" otherwise return "Noooooo"
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 8];
+function containsSeven(x) {
+    for(let i = 0; i < numbers.length; i++){
+        if (x ===}
+            
+}
+
+
+console.log(containsSeven(numbers));
+// The output should be: "Noooooo"
+// Do this again with a different solution using different list functions!
+export = containsSeven;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 'use strict';
+
+// // Write a function that checks if the array contains "7"
+// // if it contains return "Hoorray" otherwise return "Noooooo"
+// const numbers: number[] = [1, 2, 3,,7,4, 5, 6, 8];
+// function containsSeven(numbers) {
+//    if (numbers == 7){
+//        console.log('Horray')
+//    }else{console.log('Noooooo')};
+// }
+// let filteredNumber: number[]= numbers.filter(containsSeven);
+
+
+// containsSeven(numbers);
+// // The output should be: "Noooooo"
+// // Do this again with a different solution using different list functions!
+// //export = containsSeven;
