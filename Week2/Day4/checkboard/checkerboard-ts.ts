0f3fdfa068dd02f76603a6b3faa005580e3bb7d2
Alpar Fancsali <falpar@gmail.com>
2018-02-24 12:32:29 +0100

    function to center

--- Week2/Day4/checkboard/checkerboard-ts.ts
+++ Week2/Day4/checkboard/checkerboard-ts.ts
@@ -11,7 +11,7 @@ for(let x: number = 0; x < 9; x++){
     for(let y:number = 0; y < 9; y++){
         if(x % 2 === y % 2) {
             ctx.fillStyle = "black";
-            ctx.fillRect(64*x, 64*y,64,64);
+            ctx.fillRect(64*x, 64*y,64, 64);
         }else{
             ctx.fillStyle = 'white';
             ctx.fillRect(64*x, 64*y, 64, 64);
