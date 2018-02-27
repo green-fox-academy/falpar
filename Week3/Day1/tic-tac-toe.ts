9b377a2b58a9d73bbee0c6fcd28a460beb82e676
Alpar Fancsali <falpar@gmail.com>
2018-02-27 17:42:41 +0100

    tic-tac-toe

--- Week3/Day1/tic-tac-toe.ts
+++ Week3/Day1/tic-tac-toe.ts
@@ -4,6 +4,14 @@
 // open and read it. The file data represents a tic-tac-toe
 // game result. Return 'X'/'O'/'draw' based on which player
 // has winning situation.
+const fs = require('fs');
+
+function ticTacResult(parameter: string){
+     let result = fs.readFileSync(parameter, 'utf-8')
+     return result;
+}
+
+
 console.log(ticTacResult('win-o.txt'))
 // should print O
 console.log(ticTacResult('win-x.txt'))
