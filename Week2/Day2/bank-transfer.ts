3f4940de5419896b1dc6b45c6881a91564714471
Alpar Fancsali <falpar@gmail.com>
2018-03-14 14:20:39 +0100

    bank-half

---
+++ Week2/Day2/bank-transfer.ts
@@ -0,0 +1,45 @@
+'use strict';
+
+let accounts: any[] = [
+  { client_name: 'Igor', account_number: 11234543, balance: 203004099.2 },
+  { client_name: 'Vladimir', account_number: 43546731, balance: 5204100071.23 },
+  { client_name: 'Sergei', account_number: 23456311, balance: 1353600.0 }
+];
+
+// Create function that returns the name and balance of cash on an account in a list
+// The output should be: "Igor", "203004099.2"
+// Create function that transfers an amount of cash from one account to another
+// it should have four parameters:
+//  - the accounts
+//  - from account_number
+//  - to account_number
+//  - amount of cash to transfer
+//
+// Log "404 - account not found" if any of the account numbers don't exist to the console.
+// transferAmount(accounts, 43546731, 23456311, 500.0);
+//After printing the "accounts" it should look like:
+// var accounts = [
+//	{ 'client_name': 'Igor', 'account_number': 11234543, 'balance': 203004099.2 },
+//	{ 'client_name': 'Vladimir', 'account_number': 43546731, 'balance': 5204099571.23 },
+//	{ 'client_name': 'Sergei', 'account_number': 23456311, 'balance': 1354100.0 }
+//]
+
+function nameAndBalance(account: any []){
+    let accountNameandBalance: number [] = [];
+    for(let i = 0; i < account.length; i++){
+        accountNameandBalance.push(account[i].client_name);
+        accountNameandBalance.push(account[i].balance);
+
+    }
+    return accountNameandBalance;
+}
+console.log(nameAndBalance(accounts))
+
+function transfer(acoount, accountFrom, accountTo, amount) {
+    
+}
+// export = {
+//   getNameAndBalance,
+//   transferAmount,
+//   accounts
+// };
